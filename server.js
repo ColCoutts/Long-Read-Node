8815ded04098c0b65eb7102fd7d595bdbd71827e
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-13 16:38:39 -0700

    router js wired up correclty

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const http = require('http');
 const url = require('url');
-// http.createServer((request, response) => {
-//     response.setHeader('Content-Type', 'text/plain');
-//     response.write('Hello World');
-//     response.end();
-// }).listen(3000);
 
-function start() {
+function start(route) {
     function onRequest(request, response) {
         const pathname = url.parse(request.url).pathname;
         console.log('Request for ', pathname, ' received');
+
+        route(pathname);
+
         response.writeHead(200, 'Content-Type', 'text/plain');
         response.write('Hello All');
         response.end();
