103bb9e8de0eadfb013bc71419af806e739c6387
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-13 15:03:02 -0700

    passing functions/defining function in function params

--- server.js
+++ server.js
@@ -5,3 +5,17 @@ http.createServer((request, response) => {
     response.write('Hello World');
     response.end();
 }).listen(3000);
+
+//Passing Functions Around
+
+function say(word) {
+    console.log(word);
+}
+
+function execute(someFunction, value) {
+    someFunction(value);
+}
+
+execute(say, 'hello');
+
+execute(function(word){ console.log(word) }, 'Hello Dear');
