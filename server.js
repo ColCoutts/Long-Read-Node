fe540a32684007500a8c079ea83e33ddf138dc13
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-13 15:38:53 -0700

    created server module and export to index.js

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const http = require('http');
 
-http.createServer((request, response) => {
-    response.setHeader('Content-Type', 'text/plain');
-    response.write('Hello World');
-    response.end();
-}).listen(3000);
+// http.createServer((request, response) => {
+//     response.setHeader('Content-Type', 'text/plain');
+//     response.write('Hello World');
+//     response.end();
+// }).listen(3000);
 
-function onRequest(request, response) {
-    console.log('Request Received from client');
-    response.writeHead(200, 'Content-Type', 'text/plain');
-    response.write('Hello All');
-    response.end();
+function start() {
+    function onRequest(request, response) {
+        console.log('Request Received from client');
+        response.writeHead(200, 'Content-Type', 'text/plain');
+        response.write('Hello All');
+        response.end();
+    }
+    http.createServer(onRequest).listen(8888);
+    console.log('Server has started');
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log('Server has started');
+exports.start = start;
