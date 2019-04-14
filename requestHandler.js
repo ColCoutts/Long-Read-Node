3bd581151cac210f6e6feefc5a221e9c8f54fd42
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-13 17:43:58 -0700

    the not-good way for handling requests is working

--- requestHandler.js
+++ requestHandler.js
@@ -1,9 +1,11 @@
 function start() {
-    console.log('Request handler "start" was called')
+    console.log('Request handler "start" was called');
+    return 'Hello START';
 }
 
 function upload() {
     console.log('Request handler "upload" was called');
+    return 'Hello UPLOAD';
 }
 
 exports.start = start;
--- router.js
+++ router.js
@@ -2,9 +2,12 @@ function route(handle, pathname) {
     console.log('About to route request for ' + pathname);
     if(typeof handle[pathname] === 'function') {
         handle[pathname]();
+        return handle[pathname]();
     } else {
         console.log('No request handler found for ' + pathname);
+        return '404 not found';
     }
+
 }
 
 exports.route = route;
