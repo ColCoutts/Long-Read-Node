4cf92bb6027e0d69f4baf6d999dc9784ca4cccdc
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-13 17:50:11 -0700

    example of blocking

--- requestHandler.js
+++ requestHandler.js
@@ -1,5 +1,12 @@
 function start() {
     console.log('Request handler "start" was called');
+
+    function sleep(milliseconds) {
+        const startTime = new Date().getTime();
+        while (new Date().getTime < startTime + milliseconds);
+    }
+
+    sleep(10000);
     return 'Hello START';
 }
 
