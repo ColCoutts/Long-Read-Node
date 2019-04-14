c8f2b2fe4603708cac2075e05db6b1f89ea05a20
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-13 20:17:56 -0700

    trying to get image to upload

--- requestHandler.js
+++ requestHandler.js
@@ -1,32 +1,47 @@
 const querystring = require('querystring');
 const fs = require('fs');
+const formidable = require('formidable');
 
 function start(response) {
     console.log('Request handler "start" was called');
 
     const body = '<html>' +
         '<head>' +
-        '<meta http-equiv="Content-Type" content="text/html; ' +
-        'charset=UTF8" />' +
+        '<meta http-equiv="Content-Type" ' +
+        'content="text/html; charset=UTF8" />' +
         '</head>' +
         '<body>' +
-        '<form action="/upload" enctype="multipart/form-data method="post">' +
-        '<input type="file" name"upload">' +
+        '<form action="/upload" enctype="multipart/form-data ' +
+        'method="post">' +
+        '<input type="file" name="upload" multiple="multiple">' +
         '<input type="submit" value="Upload file" />' +
         '</form>' +
         '</body>' +
-        '</hthml>';
+        '</html>';
 
-        response.writeHead(200, 'Content-Type', 'text/plain');
+        response.writeHead(200, 'Content-Type', 'text/html');
         response.write(body);
         response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log('Request handler "upload" was called');
-    response.writeHead(200, 'Content-Type', 'text/plain');
-    response.write('Youve sent' + querystring.parse(postData).text);
+
+    let form = new formidable.IncomingForm();
+    console.log('about to parse');
+    form.parse(request, function(error, fields, files) {
+        console.log('parsing done');
+        fs.rename(files.upload.path, 'tmp/test.png', function(error) {
+            if(error) {
+                fs.unlink('tmp/test.png');
+                fs.rename(files.upload.path, 'tmp/test.png');
+            }
+        });
+    response.writeHead(200, 'Content-Type', 'text/html');
+    response.write('Received image: <br/>');
+    response.write('<img src= "/show" />');
     response.end();
+    });
 }
 
 function show(response) {
