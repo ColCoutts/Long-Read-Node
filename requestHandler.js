08491b2bdcaf2f2fcca9ff67cdaf4292d0e38c16
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-13 19:15:28 -0700

    have post data working

--- requestHandler.js
+++ requestHandler.js
@@ -1,21 +1,30 @@
-const exec = require('child_process').exec;
+const querystring = require('querystring');
 
 function start(response) {
     console.log('Request handler "start" was called');
 
-    exec('find /',
-    { timeout: 10000, maxBuffer: 20000*1024 },
-    function(error, stdout, stderr) {
+    const body = '<html>' +
+        '<head>' +
+        '<meta http-equiv="Content-Type" content="text/html; ' +
+        'charset=UTF8" />' +
+        '</head>' +
+        '<body>' +
+        '<form action="/upload" method="post">' +
+        '<textarea name="text" rows="20" cols="60"></textarea>' +
+        '<input type="submit" value="Submit text" />' +
+        '</form>' +
+        '</body>' +
+        '</hthml>';
+
         response.writeHead(200, 'Content-Type', 'text/plain');
-        response.write(stdout);
+        response.write(body);
         response.end();
-    });
 }
 
-function upload(response) {
+function upload(response, postData) {
     console.log('Request handler "upload" was called');
     response.writeHead(200, 'Content-Type', 'text/plain');
-    response.write('Hello UPLOAD');
+    response.write('Youve sent' + querystring.parse(postData).text);
     response.end();
 }
 
